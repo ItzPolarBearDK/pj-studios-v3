3911c6edcaf45096f56b35b4aea5332d39892b23
Patrick <patricktl2004@gmail.com>
2026-05-28 13:22:52 +0200

    Backend lobby logic added

--- TicTacToeChallenge/Backend/Program.cs
+++ TicTacToeChallenge/Backend/Program.cs
@@ -1,4 +1,7 @@
 
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Backend
 {
     public class Program
@@ -8,6 +11,13 @@ namespace Backend
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+            builder.Services.AddDbContext<AppDBContext>(options =>
+                options.UseNpgsql(connectionString));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
